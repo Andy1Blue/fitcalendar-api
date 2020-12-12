840fa83329f09a97ec5e5d9b13c117a93736b75b
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-12-12 15:57:20 +0100

    Add cors whitelist

--- src/main.ts
+++ src/main.ts
@@ -10,34 +10,44 @@ ConfigModule.forRoot({
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+  let app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     logger: console,
   });
 
-  // TODO: Add CORS settings in the future
-  //
-  // const corsWhitelist = [
-  //   'localhost',
-  //   'http://localhost',
-  //   'http://localhost:3000',
-  //   'http://localhost:3001',
-  // ];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (corsWhitelist.indexOf(origin) !== -1) {
-  //       console.log('allowed cors for:', origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log('blocked cors for:', origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: 'GET',
-  //   credentials: true,
-  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  // };
+  if (process.env.MODE === 'PROD') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const fs = require('fs');
+    const httpsOptions = {
+      key: fs.readFileSync(process.env.KEY_DIR),
+      cert: fs.readFileSync(process.env.CERT_DIR),
+      ca: fs.readFileSync(process.env.CA_DIR),
+    };
 
-  app.enableCors();
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter({ https: httpsOptions }),
+      {
+        logger: console,
+      },
+    );
+  }
+
+  const corsWhitelist = ['http://localhost:3000'];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (corsWhitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Not allowed by CORS ${origin}`));
+      }
+    },
+    allowedHeaders:
+      'token, x-www-form-urlencoded, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET, PUT, POST, DELETE, UPDATE, OPTIONS',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   const port = parseInt(process.env.PORT) || 3001;
   await app.listen(port, '0.0.0.0');
